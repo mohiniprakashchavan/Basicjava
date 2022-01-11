e6c06ee0f4e81ea6cbe770ba6b2ef45c79280f56
mohiniprakashchavan <97502911+mohiniprakashchavan@users.noreply.github.com>
2022-01-11 12:17:30 +0530

    Addition Program

---
+++ Addition.java
@@ -0,0 +1,22 @@
+
+public class Addition {
+	public static void main(String args[])
+	{
+		int a=8;
+	  int b=20;
+	  
+		int sum=a+b;
+		int Mult=a*b;
+		float Mod=a%b;
+		System.out.println("Welcome");
+
+		int Div=a/b;
+		System.out.println("sum of "+a+ " and "+b+" is="+sum);
+		System.out.println("Multiplication of "+a+ " and "+b+" is="+Mult);
+		System.out.println("Division of "+a+ " and "+b+" is="+Mod);
+		System.out.println("Mod of "+a+ " and "+b+" is="+Div);
+
+
+	}
+
+}
