343b24cfd6dec0f901a9887977221d086bae18d9
mohiniprakashchavan <97502911+mohiniprakashchavan@users.noreply.github.com>
2022-01-22 21:55:35 +0530

    Add files via upload

---
+++ Break.java
@@ -0,0 +1,22 @@
+package break_contine;
+
+public class Break {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		System.out.println("Out side for");
+		for(i=1;i<100;i++)
+		{
+	
+			if(i==50)
+			{
+				break;
+			}
+			System.out.println(i);
+		}
+		
+
+	}
+
+}
---
+++ Continue.java
@@ -0,0 +1,26 @@
+package break_contine;
+
+public class Continue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+			int i=1;
+			System.out.println("Out side for");
+			for(i=1;i<10;i++)
+			{
+		
+				if(i==5)
+				{
+					continue;
+				}
+				System.out.println(i);
+			}
+			
+
+		}
+
+	}
+
+
