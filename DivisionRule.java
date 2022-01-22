6ebc7239f5e500ecbbbec91bf8418b41d484181b
mohiniprakashchavan <97502911+mohiniprakashchavan@users.noreply.github.com>
2022-01-22 22:05:05 +0530

    Add files via upload

---
+++ DivisionRule.java
@@ -0,0 +1,35 @@
+
+public class DivisionRule {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	/*	Divide by Zero
+		int a=5;
+		int b=0;
+		int Div=a/b;
+		System.out.println(Div);
+		*/
+		
+		
+		//integer divide by 0.0
+		/*int s=5;
+		double d=0.0;
+		double d1=s/d;
+		System.out.println(d1);
+		*/
+		/* floating point divide by 0
+		float b=5;
+		int a=0;
+		float f=b/a;
+		System.out.println(f);
+		*/
+		
+		//floating point divide by 0.0
+		float f=5.5f;
+		double d=0.0;
+		double d1=f/d;
+		System.out.println(d1);
+
+	}
+
+}
